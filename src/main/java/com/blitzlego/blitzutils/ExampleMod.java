cfa2e24c271565024e90590591db0340d3416529
BlitzLego <Alec.creely@gmail.com>
2021-05-26 12:46:05 -0400

    modifed mods.toml and re-package exampleMod

--- src/main/java/com/blitzlego/blitzutils/ExampleMod.java
+++ src/main/java/com/blitzlego/blitzutils/ExampleMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.blitzlego.blitzutils;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -19,7 +19,7 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
+@Mod("blitzutils")
 public class ExampleMod
 {
     // Directly reference a log4j logger.
